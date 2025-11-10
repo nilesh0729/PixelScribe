090f4880028b691934253bbdc483766285231994
Nilesh Khodiyar <nileshkhodiyar2000@gmail.com>
2025-11-10 08:08:25 +0000

    Initialized the Schemas

---
+++ db/migration/000001_intial_schema.down.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS "performance_summary";
+DROP TABLE IF EXISTS "attempts";
+DROP TABLE IF EXISTS "dictation_settings";
+DROP TABLE IF EXISTS "settings";
+DROP TABLE IF EXISTS "dictations";
+DROP TABLE IF EXISTS "users";
