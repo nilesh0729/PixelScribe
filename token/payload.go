9ef8660c85c72b5619623d60c84b0e1c023112d1
Nilesh Khodiyar <nileshkhodiyar2000@gmail.com>
2025-12-29 18:34:20 +0530

    Implemented Authentication using Jwt Token

---
+++ token/payload.go
@@ -0,0 +1,44 @@
+package token
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+var (
+	ErrInvalidToken = errors.New("invalid token")
+	ErrExpiredToken = errors.New("expired token")
+)
+
+type Payload struct {
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	payload := &Payload{
+		Username: username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ID:        tokenID.String(),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(duration)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	return payload, nil
+}
+
+func (payload *Payload) Valid() error {
+	if time.Now().After(payload.RegisteredClaims.ExpiresAt.Time) {
+		return ErrExpiredToken
+	}
+	return nil
+}
