21d11513ee360cf100034708366fd7feb08278ca
Nilesh Khodiyar <nileshkhodiyar2000@gmail.com>
2025-12-29 15:49:05 +0530

    created confing.go for Configuration management and created password.go for hashing and verifying hashed password

---
+++ util/config.go
@@ -0,0 +1,33 @@
+package util
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DBDriver string `mapstructure:"DB_DRIVER"`
+	DBSource string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+	
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)       
+	viper.AddConfigPath(".")        
+	viper.AddConfigPath("../")      
+	viper.AddConfigPath("../..")    
+	viper.AddConfigPath("../../..") 
+
+	viper.SetConfigFile(".env")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+	err = viper.Unmarshal(&config)
+	return
+}
+
